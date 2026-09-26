c608434e2c286a6785c9a47e571753f569630fee
Itz-Me-Sumit <itzmesumit43@gmail.com>
2026-09-27 02:15:29 +0530

    ChatBot

---
+++ 3_LangGraph/ChatBot/LangGraph_BackEnd.py
@@ -0,0 +1,38 @@
+from langgraph.graph import StateGraph , START , END
+from langgraph.graph.message import BaseMessage , add_messages
+from langchain_openrouter import ChatOpenRouter
+from typing import TypedDict , Annotated
+
+
+llm = ChatOpenRouter(
+    model = "openrouter/free"
+)
+
+class ChatState(TypedDict):
+    messages : Annotated[list[BaseMessage] , add_messages]
+
+
+def chat_node(state : ChatState):
+
+    messages = state["messages"]
+
+    response = llm.invoke(messages)
+
+    return {
+        "messages" : response.content
+    }
+
+
+
+graph = StateGraph(ChatState)
+
+# Node
+graph.add_node("chat_node" , chat_node)
+
+
+# Edges
+graph.add_edge(START , "chat_node")
+graph.add_edge("chat_node" , END)
+
+graph.compile()
+
